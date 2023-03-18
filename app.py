8f7705fba88feefe9121bf5f48ac9118a5ba25f4
Arshad Khan <arshadkhan7867865422@gmail.com>
2023-03-18 14:30:36 +0530

    updates model

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pickle
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException
 # importing another python file in which inputs to be taken are defined inside a class as variable
-
+import json
 import uvicorn  # it is like a webserver for python
 import numpy as np
 import pandas as pd
@@ -22,37 +22,41 @@ class predict(BaseModel):
 app = FastAPI()
 
 
-with open("logistic_model.pkl", "rb") as f:
+with open("svc.pkl", "rb") as f:
     clf = pickle.load(f)
 
 
 #Index
 @app.get('/')
 def index():
-    return "Yo Nigga"
+    return {'hello' : "World"}
 
 #Predict
 @app.post("/predict")
 async def predict_diabetes(data: predict):
-    data = data.dict()
-    inputs = pd.DataFrame(data.items(), columns=data.keys())
+    try:
+        data_list = [[data.pregnancies, data.glucose, data.bloodpressure,
+                      data.skinthickness, data.insulin, data.bmi,
+                      data.diabetespedigreefunction, data.age]]
+        sc = StandardScaler()
+        # Reshape into 2D array and standardize
+        standardised_input = sc.fit_transform(data_list)
+
+        # Make prediction using a trained model (clf)
+        results = int(clf.predict(standardised_input))
+        
+        if results == 0:
+            return f"Prediction : {results} You are Healthy nigga"
+        elif results == 1:
+            return f"Prediction : {results} You have Diabetes , You gonna die"
     
-    # sc = StandardScaler()
-    # # #Reshaping into 2D array
-    # input = np.array(inputs).reshape(1,-1)
+        # return json.dumps(int(results))
     
-    # standardised_input = sc.fit_transfrom(input)
+    except Exception as e:
+        # Return an appropriate error message if an exception occurs
+        return {"error": str(e)}
+ 
     
-    results = clf.predict(inputs)
-    
-    return {
-        "Prediction" : 'results'
-    }
-    
-    # if input_pred == 0:
-    #     return "You are Healthy nigga"
-    # else:
-    #     return "You have Diabetes , You gonna die"
     
     
 
